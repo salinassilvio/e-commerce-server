b8c6c59f9d2deace2e88ef9f0a978f2022e86d73
Silvio Salinas <ssalinas.silvio@gmail.com>
2021-12-26 20:08:13 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e1d34558911546f60f69d14721150f0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0e871124-9a65-4c6a-b01f-e8e6d91f8363'
+};
